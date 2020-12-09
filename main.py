99aeec7b8f28d3a311e6efe174899e16638e3ea5
Shirataki2 <ahahah114514@gmail.com>
2020-12-09 21:34:41 +0900

    Fix for flake8

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-
 bot = commands.Bot(command_prefix='_')
+
 
 @bot.event
 async def on_ready():
@@ -14,5 +14,6 @@ async def on_ready():
 @bot.command()
 async def echo(ctx, *, content):
     await ctx.send(content)
+
 
 bot.run(os.environ['BOT_TOKEN'])
